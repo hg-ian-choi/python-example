6e57084b1ede01c4a8d9d48929462ad552581cd4
hg-ian-choi <hg.ian.choi@gmail.com>
2023-01-27 00:53:06 +0900

    ay: try "\n" and "str + str"

--- ay/1-print.py
+++ ay/1-print.py
@@ -1,4 +1,11 @@
-print("Hello, World!");
-print('Hello, World!');
-print("print('Hello, World!')");
-print('print("Hello, World!")');
+# "print()" function
+print("Hello, World!")
+print('Hello, World!')
+print("print('Hello, World!')")
+print('print("Hello, World!")')
+
+# "\n"
+print("Hello, World!\nHello, World!")
+
+# str + str
+print("Hello," + " " + "World!")
